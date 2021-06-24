0f0011c211db730d762de24e065e08da03abbf34
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-06-24 23:59:48 +0900

    querySelector_practice

---
+++ query-selector/index.js
@@ -0,0 +1,4 @@
+// querySelector 연습
+document.querySelector('#hello').textContent = '안녕하세요';
+document.querySelector('#world').textContent = '여러분';
+document.querySelector('#goodbye').textContent = 'Goodbye';

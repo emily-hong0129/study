b598725435ca20cbc796657ef4a576e020eb4bb7
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-07-05 20:56:48 +0900

    Add 01_transformFirstAndLast

---
+++ Algorithm_Basic/1_transformFirstAndLast.js
@@ -0,0 +1,10 @@
+function transformFirstAndLast(arr) {
+  let result = {}  // 결과값을 담을 객체
+
+  if(arr.length === 0){ // 배열 요소가 없을 때
+    return result;
+  }else{
+    result[arr[0]] = arr[arr.length - 1];
+    return result;
+  }
+}

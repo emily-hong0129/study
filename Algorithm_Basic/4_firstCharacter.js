c5a910cc6b53fd6768f41ee4046cd6dee3e7685b
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-07-06 09:33:27 +0900

    Add 04_firstCharacter

---
+++ Algorithm_Basic/4_firstCharacter.js
@@ -0,0 +1,15 @@
+function firstCharacter(str) {  // 'hello word'
+  // 각 단어마다 첫번째 글자
+  // 공백을 기준으로 나눔
+  let result = '';
+
+  if(str.length === 0){
+    return result;
+  }
+
+  let word = str.split(' ')
+  for(let i = 0; i < word.length; i++){  // ["hello", "world"]
+    result += word[i][0]
+  }
+  return result;
+}

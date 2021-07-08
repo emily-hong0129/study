d6effd49a8209d29c5e1b86ccbe483731a3d326f
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-07-08 10:50:25 +0900

    Add 08_convertDoubleSpaceToSingle

---
+++ Algorithm_Basic/8_convertDoubleSpaceToSingle.js
@@ -0,0 +1,13 @@
+function convertDoubleSpaceToSingle(str) {
+  let arr = str.split(' ');
+  let newStr = '';
+  
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === ''){
+      continue;
+    }else{
+      newStr = newStr + ' ' + arr[i];
+    }
+  }
+  return newStr.slice(1);
+}

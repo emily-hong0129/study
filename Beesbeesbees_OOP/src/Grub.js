a43dd631fb2f04927a628fd40a65006b36d1d4d4
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-07-19 22:19:55 +0900

    Add Grub

---
+++ Beesbeesbees_OOP/src/Grub.js
@@ -0,0 +1,16 @@
+class Grub {
+  // 생성자
+  constructor(age, color, food){
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+
+  // 메소드
+  eat(){
+    return 'Mmmmmmmmm jelly';
+  }
+
+}
+
+module.exports = Grub;

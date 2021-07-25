147cc06e930e6606a17fd9b81f2e4bf9dee90303
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-07-25 23:05:10 +0900

    Add 06

---
+++ Coplit/datastructure/06_Tree구현.js
@@ -0,0 +1,35 @@
+class Tree {
+  constructor(value) {
+		// constructor로 만든 객체는 트리의 Node가 된다.
+    this.value = value;
+    this.children = [];
+  }
+
+	// 트리의 삽입 메서드
+  insertNode(value) {
+		// 트리에 붙게 될 childNode를 만들고, children에 넣어야 한다.
+    const childNode = new Tree(value);
+    this.children.push(childNode);
+  }
+
+	// 트리 안에 해당 값이 포함되어 있는지 확인하는 메서드를 만든다.
+  contains(value) {
+		// 값이 포함되어 있다면 true를 반환
+    if (this.value === value) {
+      return true;
+    }
+		// 값을 찾을 때까지 children 배열을 순회하며 childNode를 탐색 (재귀사용)
+    for(let i = 0; i < this.children.length; i++){
+      let current = this.children[i];
+      if(current.value === value){
+        return true;
+      }else{
+        if(current.contains(value)){
+          return true;
+        }
+      }
+    }
+		// 전부 탐색했음에도 불구하고 찾지 못했다면 false를 반환
+    return false;
+  }
+}

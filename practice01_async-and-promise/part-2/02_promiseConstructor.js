f7980e9e80c153bde5f7f23a953a25b993d782d4
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-07-27 23:51:11 +0900

    Add 02_promiseConstructor

---
+++ practice01_async-and-promise/part-2/02_promiseConstructor.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const getDataFromFilePromise = filePath => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(data);
+      }
+    })
+  })
+};
+
+getDataFromFilePromise('README.md').then(data => console.log(data));
+
+module.exports = {
+  getDataFromFilePromise
+};

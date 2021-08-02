5c304627003fcc4e9d2e35ae3f37c686b8c6ed3c
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-08-02 23:42:33 +0900

    Add 13

---
+++ Coplit/ToyProblem/13_insertionSort.js
@@ -0,0 +1,19 @@
+const insertionSort = function (arr, transform = (item) => item) {
+  let sorted = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (transform(arr[i]) >= transform(sorted[i - 1])) {
+      sorted.push(arr[i]);
+    } else {
+      for (let j = 0; j < i; j++) {
+        if (transform(arr[i]) <= transform(sorted[j])) {
+          const left = sorted.slice(0, j);
+          const right = sorted.slice(j);
+          sorted = left.concat(arr[i], right);
+          break;
+        }
+      }
+    }
+  }
+
+  return sorted;
+};

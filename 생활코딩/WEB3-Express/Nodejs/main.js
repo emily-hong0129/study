a27a98cf0a4b518ba4cc8f24946a1cf68a263098
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-08-08 23:30:09 +0900

    Update file

--- 생활코딩/WEB3-Express/Nodejs/main.js
+++ 생활코딩/WEB3-Express/Nodejs/main.js
@@ -94,8 +94,49 @@ app.post('/create_process', (request, response) => {
 })
 
 app.get('/update/:pageId', (request, response) => {
-
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var list = template.list(filelist);
+      var html = template.HTML(title, list,
+        `
+        <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      );
+      response.send(html);
+    });
+  });
 });
+
+app.post('/update_process', (request, response) => {
+  var body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function(error){
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+        })
+      });
+  });
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));

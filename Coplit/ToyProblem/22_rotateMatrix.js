74c836ad7858ec66b808365f54e9d0634521ba77
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-08-23 19:43:46 +0900

    Add 22

--- Coplit/ToyProblem/22_rotateMatrix.js
+++ Coplit/ToyProblem/22_rotateMatrix.js
@@ -1,3 +1,39 @@
+const rotateMatrix = function (matrix, k= 1) {
+  /*
+  [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+  ]
+ */
+
+  // 빈배열인 경우
+  if(matrix.length === 0) return matrix;
+
+  // k값 받아서 k번 회전시킨 배열
+  for(let i = 0; i < k; i++){
+    matrix = rotate(matrix)
+  }
+  return matrix
+
+};
+
+const rotate = (matrix) => {
+  let result = [];
+  for(let i = 0; i < matrix[0].length; i++) {
+    let array = [] //새로운 빈 배열 선언
+    for(let j = matrix.length-1; j >= 0; j--){
+      array.push(matrix[j][i])
+    } // [13, 9, 5, 1] -> [14, 10, 6, 2]
+    result.push(array)
+  }
+
+  return result
+}
+
+
+/*
 const rotateMatrix = function (matrix, rotateNum = 1) {
   // rotateNum 이 0일 수 있으므로 아래와 같은 초기화는 지양해야 함
   // rotateNum = rotateNum || 1
@@ -25,3 +61,4 @@ const rotateMatrix = function (matrix, rotateNum = 1) {
 
   return rotated;
 };
+*/

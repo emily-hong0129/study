ceb0ee6ebeed9a9900bf4086ed19830e384daf4b
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-08-25 01:35:11 +0900

    Add 01

--- Coplit/알고리즘/01_짐_나르기.js
+++ Coplit/알고리즘/01_짐_나르기.js
@@ -1,8 +1,7 @@
 function movingStuff(stuff, limit) {  //  [70, 50, 80, 50], 100 => 3
-  let sortedArr = stuff.sort((a,b) => a-b); // [80, 70, 50, 50]
+  let sortedArr = stuff.sort((a,b) => a-b);
   let count = 0;  // 나갈때마다 카운트
 
-  // 배열을 순회
   while (sortedArr.length !==0) { 
     if (sortedArr[0] + sortedArr[sortedArr.length-1] <= limit) { 
       count++ 

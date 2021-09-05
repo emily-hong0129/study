e979e8e3c364d81a60af4dabb29e5faaec0002de
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-09-05 23:48:33 +0900

    Update file

--- shortly-mvc/routes/links.js
+++ shortly-mvc/routes/links.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/links/index')
+const controller = require('../controllers/links/index');
 
 /* GET links listing. */
 // router.get('/', function (req, res, next) {
-//   res.send('??');
+//   res.send('respond with a resource');
 // });
-
-// https://expressjs.com/ko/starter/basic-routing.html
 
 // get
 router.get('/', controller.get)
-
 // getId
-// 화면에 나옴
 router.get('/:id', controller.getId)
-
 // post
 router.post('/', controller.post)
-
 
 module.exports = router;

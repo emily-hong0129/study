9201f1e64d1e32de055fe43a59b9db9e3aa2a5f9
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-09-11 23:42:42 +0900

    Add file

--- auth-oauth/server-oauth/index.js
+++ auth-oauth/server-oauth/index.js
@@ -1,43 +1,23 @@
-require("dotenv").config();
-const fs = require("fs");
-const https = require("https");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 8080;
 
-const controllers = require("./controllers");
+const handleCallback = require('./controller/callback')
+const handleImages = require('./controller/images')
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-    methods: ["GET", "POST", "OPTIONS"],
-  })
+  cors({ origin: true })
 );
-app.use(cookieParser());
-app.post("/login", controllers.login);
-app.get("/accesstokenrequest", controllers.accessTokenRequest);
-app.get("/refreshtokenrequest", controllers.refreshTokenRequest);
 
-const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+app.post('/callback', handleCallback);
 
-// 인증서 파일들이 존재하는 경우에만 https 프로토콜을 사용하는 서버를 실행합니다. 
-// 만약 인증서 파일이 존재하지 않는경우, http 프로토콜을 사용하는 서버를 실행합니다.
-// 파일 존재여부를 확인하는 폴더는 서버 폴더의 package.json이 위치한 곳입니다.
-let server;
-if(fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")){
+app.get('/images', handleImages)
 
-  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-  const credentials = { key: privateKey, cert: certificate };
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
 
-  server = https.createServer(credentials, app);
-  server.listen(HTTPS_PORT, () => console.log("server runnning"));
-
-} else {
-  server = app.listen(HTTPS_PORT)
-}
-module.exports = server;
+module.exports = app;
